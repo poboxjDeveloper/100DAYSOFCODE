5232b10df6c4b121a0df1fbb53a8775d8c5cf130
Mr Test Aankoop <testaankoop.poboxj@gmail.com>
2022-03-24 11:10:15 +0100

    feature - lean api - add the project

---
+++ LEARN-API-USE/my-imd-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="info"></section>
+      <header>
+        <h1>IMDB search</h1>
+      </header>
+      <details>
+        <summary>System Requirements</summary>
+        <p>
+          Requires a computer running an operating system. The computer must
+          have some memory and ideally some kind of long-term storage. An input
+          device as well as some form of output device is recommended.
+        </p>
+      </details>
+    </>
+  );
+}
+
+export default App;
