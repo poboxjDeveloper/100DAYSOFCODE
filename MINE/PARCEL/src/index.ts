a112236ee219f4090ec7b5ca7e40bdbc6e1df217
Mr Test Aankoop <testaankoop.poboxj@gmail.com>
2022-04-06 17:20:06 +0200

    feat - parcel - partial commit of the parcel branch

--- MINE/PARCEL/src/index.ts
+++ MINE/PARCEL/src/index.ts
@@ -6,3 +6,14 @@ let playground = qs("section .playground");
 playground.innerHTML = "This is the playground #001";
 playground.style.backgroundColor = "#ff0";
 playground.style.padding = ".5em";
+
+let ol = document.createElement("ol");
+let li = document.createElement("li");
+
+ol.setAttribute("value", "3");
+
+// Create a array of the attributes NamedNodeMap which is a collection of attributes but not an array
+let attr = ol.attributes;
+[...attr].forEach((a) => {
+  console.log(a.name, a.value);
+});
