b3f33e8e9629fd19ddb40612f9f69263c3808c8e
Mr Test Aankoop <testaankoop.poboxj@gmail.com>
2022-05-13 12:58:07 +0200

    f - svg - update the positioning of the pointer

--- SVG/src/ecc-topcorner.js
+++ SVG/src/ecc-topcorner.js
@@ -8,14 +8,67 @@
     shape.setAttribute("viewBox", "0 0 100 100");
   });
 
+  function positionShape(box) {
+    let shapeContainer = document.getElementById("shape_container"),
+      theShape = shapeContainer.firstElementChild,
+      theShapeG = theShape.firstElementChild,
+      theShapeDimensions = theShapeG.getBoundingClientRect(),
+      style = `position:absolute; top:${Math.round(
+        box.bottom
+      )}px; left:${Math.round(
+        box.left + box.width / 2 - theShapeDimensions.width / 2
+      )}px; width:${Math.round(theShapeDimensions.width)}px`;
+
+    shapeContainer.setAttribute("style", style);
+
+    console.log("g:", theShapeDimensions, "box", box);
+
+    // let
+    // container = document.getElementById("subnav_container"),
+    // old = document.getElementById("cloned_shape"),
+    //   shape = document.getElementById("svg_to_clone"),
+
+    //   clone = shape.cloneNode(true),
+    //   style = `position:absolute; top:${Math.round(
+    //     box.bottom
+    //   )}px; left:${Math.round(box.left - 150 / 2)}px; width:150px; `;
+
+    // if (old) {
+    //   old.remove();
+    // }
+
+    // clone.id = "";
+    // container.id = "cloned_shape";
+    // container.setAttribute("class", "svg-container");
+    // container.setAttribute("style", style);
+    // container.appendChild(clone);
+
+    // document.body.appendChild(container);
+
+    // console.log(box, clone);
+  }
+
   // get the coordinates of the clicked item
   let a = document.querySelectorAll("nav ul li a");
   for (let i = 0; i < a.length; i++) {
     a[i].addEventListener("click", function (e) {
       e.preventDefault();
       let x = e.clientX,
-        y = e.clientY;
-      console.log("x", x, "y", y, e.currentTarget, e.currentTarget.offsetWidth);
+        y = e.clientY,
+        box = e.target.getBoundingClientRect();
+      positionShape(box);
+
+      //   console.log(
+      //     "box",
+      //     box,
+      //     "x",
+      //     x,
+      //     "y",
+      //     y,
+      //     e.currentTarget,
+      //     e.currentTarget.offsetWidth,
+      //     e.currentTarget.offsetHeight
+      //   );
     });
   }
 })();
